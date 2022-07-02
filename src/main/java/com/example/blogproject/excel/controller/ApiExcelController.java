a52ce82cad49837cfa10144ea65cb599303f60e5
jeonghyeonkwon <givejeong2468@gmail.com>
2022-07-02 16:50:11 +0900

    엑셀 다운로드 기능 잠시 보류

--- src/main/java/com/example/blogproject/excel/controller/ApiExcelController.java
+++ src/main/java/com/example/blogproject/excel/controller/ApiExcelController.java
@@ -3,6 +3,7 @@ package com.example.blogproject.excel.controller;
 import com.example.blogproject.excel.dto.download.RequestDto;
 import com.example.blogproject.excel.util.ExcelDownLoadUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,14 +16,14 @@ import javax.servlet.http.HttpServletResponse;
 public class ApiExcelController {
 
     private final ExcelDownLoadUtil excelDownLoadUtil;
-    @PostMapping("/download")
-    public void excelDownload(@RequestBody RequestDto dto, HttpServletResponse res){
-        System.out.println(dto);
-        try{
-            excelDownLoadUtil.excelDownLoad(dto,res);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-
-    }
+//    @PostMapping("/download")
+//    public ResponseEntity<Resource> excelDownload(@RequestBody RequestDto dto, HttpServletResponse res){
+//        System.out.println(dto);
+//        try{
+//            return excelDownLoadUtil.excelDownLoad(dto,res);
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//
+//    }
 }
--- src/main/java/com/example/blogproject/excel/util/ExcelDownLoadUtil.java
+++ src/main/java/com/example/blogproject/excel/util/ExcelDownLoadUtil.java
@@ -6,6 +6,9 @@ import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriUtils;
 
@@ -16,27 +19,28 @@ import java.util.List;
 
 @Component
 public class ExcelDownLoadUtil {
-    public void excelDownLoad(RequestDto dto, HttpServletResponse res) throws IOException {
-        XSSFWorkbook wb = new XSSFWorkbook();
-        XSSFSheet sheet = wb.createSheet("시트 명 적는곳 입니다.");
-        XSSFRow row = null;
-        XSSFCell cell = null;
-        int headerStartRow = 1;
-        int targetCell = 0;
-        List<String> titleList = dto.getTitleList();
-        List<DataDto> dataList = dto.getDataList();
-        row = sheet.createRow(headerStartRow++);
-        for(int i=0;i<titleList.size();i++){
-            cell = row.createCell(i);
-            cell.setCellValue(titleList.get(i));
-            sheet.setColumnWidth(i,2000);
-        }
+//    public ResponseEntity<Resource> excelDownLoad(RequestDto dto, HttpServletResponse res) throws IOException {
+//                XSSFWorkbook wb = new XSSFWorkbook();
+//        XSSFSheet sheet = wb.createSheet("시트 명 적는곳 입니다.");
+//        XSSFRow row = null;
+//        XSSFCell cell = null;
+//        int headerStartRow = 1;
+//        int targetCell = 0;
+//        List<String> titleList = dto.getTitleList();
+//        List<DataDto> dataList = dto.getDataList();
+//        row = sheet.createRow(headerStartRow++);
+//        for(int i=0;i<titleList.size();i++){
+//            cell = row.createCell(i);
+//            cell.setCellValue(titleList.get(i));
+//            sheet.setColumnWidth(i,2000);
+//        }
 
-        res.setContentType("application/vnd.ms-excel");
-        String fileName = UriUtils.encode("abc.xlsx", StandardCharsets.UTF_8);
-        res.setHeader("Content-Disposition","attachment; filename=\""+fileName+"\"");
+//        res.setContentType("application/vnd.ms-excel");
+//        String fileName = UriUtils.encode("abc.xlsx", StandardCharsets.UTF_8);
+//        res.setHeader("Content-Disposition","attachment; filename=\""+fileName+"\"");
+//
+//        wb.write(res.getOutputStream());
 
-        wb.write(res.getOutputStream());
-
-    }
+//        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION).body(wb.get);
+//    }
 }
