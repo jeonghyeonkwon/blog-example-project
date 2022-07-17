56519854f5ab40a0e58916621358e956ffbb22f1
jeonghyeonkwon <givejeong2468@gmail.com>
2022-07-18 00:01:28 +0900

    webClient 호출 예제 완성

--- src/main/java/com/example/blogproject/aboutapi/WebConfig.java
+++ src/main/java/com/example/blogproject/aboutapi/WebConfig.java
@@ -3,12 +3,19 @@ package com.example.blogproject.aboutapi;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebConfig {
     @Bean
     public RestTemplateBuilder restTemplateBuilder(){
         RestTemplateBuilder builder = new RestTemplateBuilder();
+        return builder;
+    }
+
+    @Bean
+    public WebClient.Builder webClientBuilder(){
+        WebClient.Builder builder = WebClient.builder();
         return builder;
     }
 }
--- src/main/java/com/example/blogproject/aboutapi/controller/ApiCallController.java
+++ src/main/java/com/example/blogproject/aboutapi/controller/ApiCallController.java
@@ -6,26 +6,60 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/call")
 public class ApiCallController {
     private final RestTemplate restTemplate;
+    private final WebClient webClient;
     private final String EXPRESS_URL = "http://localhost:8000/api";
 
-    public ApiCallController(RestTemplateBuilder restTemplateBuilder){
+    public ApiCallController(RestTemplateBuilder restTemplateBuilder, WebClient.Builder webClientBuilder){
         this.restTemplate = restTemplateBuilder.build();
+        this.webClient = webClientBuilder.build();
     }
     @PostMapping ("/rest-template")
-    public ResponseEntity call(@RequestBody ApiRequestDto dto){
+    public ResponseEntity callRestTemplate(@RequestBody ApiRequestDto dto){
         try{
-            ResponseEntity<ApiRequestDto> stringResponseEntity = restTemplate.postForEntity(EXPRESS_URL+"/receive", dto, ApiRequestDto.class);
+            ResponseEntity<ApiRequestDto> stringResponseEntity = restTemplate
+                    .postForEntity(EXPRESS_URL+"/receive",
+                            dto,
+                            ApiRequestDto.class
+                    );
             System.out.println(stringResponseEntity.getBody());
             return new ResponseEntity(stringResponseEntity.getBody(),HttpStatus.OK);
         }catch (Exception e){
             e.printStackTrace();
             return new ResponseEntity("잘못 요청했습니다.",HttpStatus.BAD_REQUEST);
         }
+    }
+
+    /*
+    * 도움되는 글 :
+    * https://gngsn.tistory.com/154
+    * https://www.baeldung.com/spring-webclient-get-response-body
+    * */
+    @PostMapping("/web-client")
+    public ResponseEntity callWebClient(@RequestBody ApiRequestDto dto){
+        try{
+
+            // mono 0~1
+            // flux 0~N
+            ApiRequestDto block = webClient.post()
+                    .uri(EXPRESS_URL + "/receive")
+                    .body(Mono.just(dto), ApiRequestDto.class)
+                    .retrieve()
+                    .bodyToMono(ApiRequestDto.class).block();
+
+            System.out.println(block);
+            return new ResponseEntity(block,HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity("잘못 요청했습니다.",HttpStatus.BAD_REQUEST);
+        }
+
     }
 
 }
