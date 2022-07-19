8a308205a387de5f0cf24198b7eec09a2ed77a8a
jeonghyeonkwon <givejeong2468@gmail.com>
2022-07-19 13:44:49 +0900

    express 데이터 변환 후 전달 예제 작성

--- src/main/java/com/example/blogproject/aboutapi/controller/ApiReceiveController.java
+++ src/main/java/com/example/blogproject/aboutapi/controller/ApiReceiveController.java
@@ -1,4 +1,26 @@
 package com.example.blogproject.aboutapi.controller;
 
+import com.example.blogproject.aboutapi.dto.ApiRequestDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/receive")
 public class ApiReceiveController {
+    @PostMapping("")
+    public ResponseEntity apiRequest(@RequestBody ApiRequestDto dto){
+        System.out.println(dto);
+        String userName = dto.getUserName() + " 응답 완료";
+        List<String> userArray = dto.getUserArray().stream().map(data -> data + " 응답 완료").collect(Collectors.toList());
+
+        ApiRequestDto response = new ApiRequestDto(userName,userArray);
+        return new ResponseEntity(response,HttpStatus.OK);
+    }
 }
