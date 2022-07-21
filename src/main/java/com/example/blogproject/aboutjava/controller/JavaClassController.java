9fb86d6b9b60be9a38fec1fe308eb916d6cf2846
jeonghyeonkwon <givejeong2468@gmail.com>
2022-07-21 16:18:15 +0900

    자바 outerclass innerclass 관련 예제 생성 추가 예정

---
+++ src/main/java/com/example/blogproject/aboutjava/controller/JavaClassController.java
@@ -0,0 +1,52 @@
+package com.example.blogproject.aboutjava.controller;
+
+import com.example.blogproject.aboutjava.dto.OuterClass;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/java/class")
+public class JavaClassController {
+    @GetMapping("/outer-class")
+    public ResponseEntity outerClassTest(){
+        OuterClass outerClass = new OuterClass("outerClassTest");
+        OuterClass.InnerClass innerClass = outerClass.new InnerClass("innerClassTest");
+        controllerPrint("innerClass",innerClass);
+        innerClass.print("controller","controller","컨트롤러에서 매소드 호출");
+//        OuterClass.InnerClass("innerClassTest");
+        OuterClass.StaticInnerClass staticInnerClass = new OuterClass.StaticInnerClass("staticInnerClass");
+        controllerPrint("staticInnerClass",staticInnerClass);
+        return new ResponseEntity(HttpStatus.OK);
+        /*
+        *
+        * className= OuterClass fieldName= finalOuterField fieldValue= staticOuterField
+        * className= OuterClass fieldName= outerField fieldValue= outerClassTest
+        * className= OuterClass fieldName= staticInnerField fieldValue= staticInnerField
+        * className= InnerClass fieldName= staticOuterField fieldValue= staticOuterField
+        * className= InnerClass fieldName= outerField fieldValue= outerClassTest
+        * className= InnerClass fieldName= defaultInnerField fieldValue= defaultInnerField
+        * className= InnerClass fieldName= innerField fieldValue= innerClassTest
+        * ---------------innerClass---------------
+        * OuterClass.InnerClass(defaultInnerField=defaultInnerField, innerField=innerClassTest)
+        * className= controller fieldName= controller fieldValue= 컨트롤러에서 매소드 호출
+        * className= staticInnerClass fieldName= finalOuterField fieldValue= staticOuterField
+        * className= staticInnerClass fieldName= staticInnerField fieldValue= staticInnerField
+        * className= staticInnerClass fieldName= innerField fieldValue= staticInnerClass
+        * ---------------staticInnerClass---------------
+        * OuterClass.StaticInnerClass(innerField=staticInnerClass)
+        * */
+    }
+    @PostMapping("/outer-class")
+    public ResponseEntity requestOuterClass(@RequestBody OuterClass outerClass){
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    public void controllerPrint(String fieldName,Object obj){
+        System.out.println("---------------"+fieldName+"---------------");
+        System.out.println(obj);
+    }
+
+
+
+}
---
+++ src/main/java/com/example/blogproject/aboutjava/dto/OuterClass.java
@@ -0,0 +1,67 @@
+package com.example.blogproject.aboutjava.dto;
+
+import lombok.ToString;
+
+@ToString
+public class OuterClass {
+    private static String staticOuterField = "staticOuterField";
+
+    private String outerField;
+
+    public OuterClass(String outerField){
+
+        this.outerField = outerField;
+
+//        InnerClass.print("OuterClass","finalOuterField",this.staticOuterField);
+//        InnerClass.print("OuterClass","outerField",this.outerField);
+//        InnerClass.print("OuterClass","staticInnerField", staticInnerField);
+//        InnerClass.print("finalOuterField","innerField",innerField);
+
+        StaticInnerClass.staticPrint("OuterClass","finalOuterField",this.staticOuterField);
+        StaticInnerClass.staticPrint("OuterClass","outerField",this.outerField);
+        StaticInnerClass.staticPrint("OuterClass","staticInnerField", StaticInnerClass.staticInnerField);
+//        StaticInnerClass.print("finalOuterField","innerField",innerField); //컴파일 요류
+    }
+    /*
+
+    * https://www.youtube.com/watch?v=C6CczyrkYXU&t=902s
+    * innerClass로 선언하면 outerClass에 모든 값들을 참조 할 수 있으므로 의존성이 강해진다
+    * innerClass 역할이 끝나고 gc가 제 역할을 수행하지 못함
+    * */
+    @ToString
+    public class InnerClass{
+        private  String defaultInnerField = "defaultInnerField";
+        private String innerField;
+        public InnerClass(String innerField){
+            this.innerField = innerField;
+            print("InnerClass","staticOuterField",staticOuterField);
+            print("InnerClass","outerField",outerField);
+            print("InnerClass","defaultInnerField",this.defaultInnerField);
+            print("InnerClass","innerField",this.innerField);
+        }
+        public void print(String className,String fieldName,String fieldValue){
+            System.out.printf("className= %s fieldName= %s fieldValue= %s\n",className,fieldName,fieldValue);
+        }
+    }
+    /*
+    * 만약 static 클래스로 선언한것이 외부 인스턴스를 참조하지 않는다면
+    * 임시적 참조를 유지하지 않는다
+    * */
+    @ToString
+    public static class StaticInnerClass{
+        private static String staticInnerField = "staticInnerField";
+        private String innerField;
+        public StaticInnerClass(String innerField){
+            this.innerField = innerField;
+            staticPrint("staticInnerClass","finalOuterField",staticOuterField);
+//            staticPrint("staticInnerClass","outerField",outerField); //컴파일 오류
+            staticPrint("staticInnerClass","staticInnerField",this.staticInnerField);
+            staticPrint("staticInnerClass","innerField",this.innerField);
+        }
+        public static void staticPrint(String className,String fieldName,String fieldValue){
+
+            System.out.printf("className= %s fieldName= %s fieldValue= %s\n",className,fieldName,fieldValue);
+        }
+    }
+
+}
